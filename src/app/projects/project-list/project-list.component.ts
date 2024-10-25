43c3cd0475e9b09282082d1365448a65debade5c
Kaustubh <kkelvekar.5@hotmail.com>
2024-10-25 22:04:42 +0100

    Paging added

--- src/app/projects/project-list/project-list.component.ts
+++ src/app/projects/project-list/project-list.component.ts
@@ -1,13 +1,12 @@
 // project-list.component.ts
 import { Component, Input, OnInit } from '@angular/core';
-import { SonarQubeProjectGroupData } from '../../shared/services/sonarqube-project.data';
-import { SonarQubeProjectDataService } from '../../shared/services/sonarqube-project-data.service';
 import { FilterCriteria } from '../shared/interfaces/filter-criteria';
 import { FilteringService } from '../shared/services/filtering.service';
 import { SortingService } from '../shared/services/sorting.service';
 import { ProjectDataService } from '../shared/services/project.data.service';
 import { ProjectList } from './project-list';
-
+import { ProjectItem } from './project-item/project-item';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-project-list',
@@ -19,28 +18,70 @@ export class ProjectListComponent implements OnInit {
   @Input() filterCriteria: FilterCriteria = {};
 
   projectList: ProjectList[] = [];
+  flattenedProjectItems: ProjectItem[] = [];
   noProjectsFound: boolean = false;
+
+  currentPage: number = 1;
+  itemsPerPage: number = 2;
+  totalItems: number = 0;
 
   constructor(
     private projectDataService: ProjectDataService,
     private filteringService: FilteringService,
-    private sortingService: SortingService // Inject SortingService
+    private sortingService: SortingService
   ) { }
 
   ngOnInit() {
     this.projectDataService.projectList$.subscribe(data => {
       this.projectList = data;
+      this.flattenedProjectItems = this.flattenProjectItems(data);
+      this.totalItems = this.flattenedProjectItems.length;
     });
   }
 
   get processedProjectGroupList(): ProjectList[] {
-    let _processedProjectGroupList = this.projectList || [];
+    let processedList = this.applyFiltersAndSorting(this.projectList || []);
+    this.noProjectsFound = processedList.length === 0;
+    let paginatedItems = this.paginate(this.flattenProjectItems(processedList));
+    return this.groupByProjectGroup(paginatedItems);
+  }
 
-    _processedProjectGroupList = this.filteringService.applyFilters(_processedProjectGroupList, this.filterCriteria);
-    _processedProjectGroupList = this.sortingService.sortProjectsWithinGroups(_processedProjectGroupList, this.filterCriteria.sortBy); // Use SortingService
-    _processedProjectGroupList = this.sortingService.sortGroupsByMetric(_processedProjectGroupList, this.filterCriteria.sortBy); // Use SortingService
+  private applyFiltersAndSorting(projectList: ProjectList[]): ProjectList[] {
+    let filteredList = this.filteringService.applyFilters(projectList, this.filterCriteria);
+    filteredList = this.sortingService.sortProjectsWithinGroups(filteredList, this.filterCriteria.sortBy);
+    return this.sortingService.sortGroupsByMetric(filteredList, this.filterCriteria.sortBy);
+  }
 
-    this.noProjectsFound = _processedProjectGroupList.length === 0;
-    return _processedProjectGroupList;
+  private flattenProjectItems(projectList: ProjectList[]): any[] {
+    return _.flatMap(projectList, projects =>
+      projects.projectItems.map(projectItem => ({
+        ...projectItem,
+        projectGroupName: projects.projectGroup.name,
+        projectGroup: projects.projectGroup
+      }))
+    );
+  }
+
+  private paginate(items: ProjectItem[]): any[] {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    return items.slice(start, end);
+  }
+
+  private groupByProjectGroup(items: any[]): ProjectList[] {
+    return _.chain(items)
+      .groupBy('projectGroupName')
+      .map((projectItems, groupName) => ({
+        projectGroup: projectItems[0].projectGroup,
+        projectItems: projectItems.map(item => {
+          const { projectGroupName, projectGroupMetrics, ...rest } = item;
+          return rest;
+        })
+      }))
+      .value() as ProjectList[];
+  }
+
+  onPageChange(page: number) {
+    this.currentPage = page;
   }
 }
