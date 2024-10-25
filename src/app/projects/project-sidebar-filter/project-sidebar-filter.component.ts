32d0595f623e2017aa9e52207aa1652b96242358
Kaustubh <kkelvekar.5@hotmail.com>
2024-10-25 23:50:50 +0100

    Added dynamic badges count in sidebar filter

--- src/app/projects/project-sidebar-filter/project-sidebar-filter.component.ts
+++ src/app/projects/project-sidebar-filter/project-sidebar-filter.component.ts
@@ -1,5 +1,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FilterCriteria } from '../shared/interfaces/filter-criteria';
+import { ProjectList } from '../project-list/project-list';
+import { ProjectDataService } from '../shared/services/project.data.service';
 
 @Component({
   selector: 'project-sidebar-filter',
@@ -8,15 +10,120 @@ import { FilterCriteria } from '../shared/interfaces/filter-criteria';
 })
 export class ProjectSidebarFilterComponent {
   filterCriteria: FilterCriteria = {};
+  projectList: ProjectList[] = [];
+
+  coverageCounts = {
+    '>=80': 0,
+    '70-80': 0,
+    '50-70': 0,
+    '30-50': 0,
+    '<30': 0
+  };
+
+  ratingCounts = {
+    reliability: { 'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0 },
+    security: { 'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0 },
+    maintainability: { 'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0 }
+  };
 
   @Output() filterChange = new EventEmitter<FilterCriteria>();
 
   selectedQualityGate: 'passed' | 'failed' | null = null;
   selectedRatings: { [category: string]: Set<string> } = {};
+  selectedCoverage: string[] = [];
+
+  constructor(
+    private projectDataService: ProjectDataService,
+  ) { }
+
+  ngOnInit() {
+    this.projectDataService.projectList$.subscribe(data => {
+      this.projectList = data;
+      this.calculateCounts();
+      // console.log(this.projectList);
+    });
+  }
+
+  calculateCounts() {
+    // Reset counts to zero
+    this.coverageCounts = {
+      '>=80': 0,
+      '70-80': 0,
+      '50-70': 0,
+      '30-50': 0,
+      '<30': 0
+    };
+
+    this.ratingCounts = {
+      reliability: { 'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0 },
+      security: { 'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0 },
+      maintainability: { 'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0 }
+    };
+
+    this.projectList.forEach(projectListItem => {
+      projectListItem.projectItems.forEach(projectItem => {
+        // Calculate coverage counts
+        const coverage: number = parseFloat(projectItem.coverage.toString().replace('%', '') || '0');
+        if (coverage >= 80) {
+          this.coverageCounts['>=80']++;
+        } else if (coverage >= 70) {
+          this.coverageCounts['70-80']++;
+        } else if (coverage >= 50) {
+          this.coverageCounts['50-70']++;
+        } else if (coverage >= 30) {
+          this.coverageCounts['30-50']++;
+        } else {
+          this.coverageCounts['<30']++;
+        }
+
+        // Increment maintainability rating counts
+        const sqale_rating = projectItem.sqale_rating;
+        if (sqale_rating === 'A') {
+          this.ratingCounts.maintainability['A']++;
+        } else if (sqale_rating === 'B') {
+          this.ratingCounts.maintainability['B']++;
+        } else if (sqale_rating === 'C') {
+          this.ratingCounts.maintainability['C']++;
+        } else if (sqale_rating === 'D') {
+          this.ratingCounts.maintainability['D']++;
+        } else if (sqale_rating === 'E') {
+          this.ratingCounts.maintainability['E']++;
+        }
+
+        // Increment security rating counts
+        const security_rating = projectItem.security_rating;
+        if (security_rating === 'A') {
+          this.ratingCounts.security['A']++;
+        } else if (security_rating === 'B') {
+          this.ratingCounts.security['B']++;
+        } else if (security_rating === 'C') {
+          this.ratingCounts.security['C']++;
+        } else if (security_rating === 'D') {
+          this.ratingCounts.security['D']++;
+        } else if (security_rating === 'E') {
+          this.ratingCounts.security['E']++;
+        }
+
+        // Increment reliability rating counts
+        const reliability_rating = projectItem.reliability_rating;
+        if (reliability_rating === 'A') {
+          this.ratingCounts.reliability['A']++;
+        } else if (reliability_rating === 'B') {
+          this.ratingCounts.reliability['B']++;
+        } else if (reliability_rating === 'C') {
+          this.ratingCounts.reliability['C']++;
+        } else if (reliability_rating === 'D') {
+          this.ratingCounts.reliability['D']++;
+        } else if (reliability_rating === 'E') {
+          this.ratingCounts.reliability['E']++;
+        }
+      });
+    });
+  }
 
   // Handle Quality Gate selection
   onQualityGateChange(status: 'passed' | 'failed') {
-    if (this.selectedQualityGate === status) {
+      if(this.selectedQualityGate === status) {
       // Deselect if already selected
       this.selectedQualityGate = null;
       this.filterCriteria.qualityGate = undefined;
@@ -62,6 +169,20 @@ export class ProjectSidebarFilterComponent {
         this.filterCriteria.maintainabilityRatings = ratings.length > 0 ? ratings : undefined;
         break;
       // Add cases for other categories if needed
+    }
+    this.emitFilterChange();
+  }
+
+  isCoverageSelected(range: string): boolean {
+    return this.selectedCoverage.includes(range);
+  }
+
+  toggleCoverage(range: string): void {
+    const index = this.selectedCoverage.indexOf(range);
+    if (index > -1) {
+      this.selectedCoverage.splice(index, 1);
+    } else {
+      this.selectedCoverage.push(range);
     }
     this.emitFilterChange();
   }
